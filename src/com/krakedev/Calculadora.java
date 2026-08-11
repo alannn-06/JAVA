cf0b643bf67ef0aa7243e6737aa1128a9ae8a7a9
alannn-06 <alanchanatasig2006@gmail.com>
2026-08-10 21:14:09 -0500

    Funciones multiplicar, dividir, promediar, y mostrarResultado

---
+++ src/com/krakedev/Calculadora.java
@@ -0,0 +1,39 @@
+package com.krakedev;
+
+public class Calculadora {
+
+    public int sumar(int a, int b) {
+        int resultado;
+        resultado = a + b;
+        return resultado;
+    }
+
+    public int restar(int a, int b) {
+        int resultado;
+        resultado = a - b;
+        return resultado;
+    }
+
+    public double multiplicar(double valor1, double valor2) {
+        double resultado;
+        resultado = valor1 * valor2;
+        return resultado;
+    }
+
+    public double dividir(double dividendo, double divisor) {
+        double resultado;
+        resultado = dividendo / divisor;
+        return resultado;
+    }
+
+    public double promediar(double valor1, double valor2, double valor3) {
+        double resultado;
+        resultado = (valor1 + valor2 + valor3) / 3;
+        return resultado;
+    }
+
+    public void mostrarResultado() {
+        System.out.println("Ahorita no joven, sali al almuerzo y regreso en 15 minutos");
+    }
+
+}
---
+++ src/com/krakedev/TestCalculadora.java
@@ -0,0 +1,31 @@
+package com.krakedev;
+
+public class TestCalculadora {
+
+    public static void main(String[] args) {
+        Calculadora c1 = new Calculadora();
+
+        int r1;
+        r1 = c1.sumar(4, 2);
+        System.out.println("Resultado Suma: " + r1);
+
+        int r2;
+        r2 = c1.restar(4, 2);
+        System.out.println("Resultado Resta: " + r2);
+
+        double resultadoMultiplicar;
+        resultadoMultiplicar = c1.multiplicar(10.0, 5.0);
+        System.out.println("Resultado Multiplicación: " + resultadoMultiplicar);
+
+        double resultadoDividir;
+        resultadoDividir = c1.dividir(10.0, 2.0);
+        System.out.println("Resultado División: " + resultadoDividir);
+
+        double resultadoPromedio;
+        resultadoPromedio = c1.promediar(10.0, 8.0, 9.0);
+        System.out.println("Resultado Promedio: " + resultadoPromedio);
+
+        c1.mostrarResultado();
+    }
+
+}
